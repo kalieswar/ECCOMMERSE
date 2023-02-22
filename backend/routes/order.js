72bafe65ad1b83244195044de13a7b0a332c75f6
kalieswar <kaliesbhuvanesh25@gmail.com>
2023-02-22 17:33:23 +0530

    new commit

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -8,8 +8,8 @@ router.route('/order/:id').get(isAuthenticatedUser,getSingleOrder);
 router.route('/myorders').get(isAuthenticatedUser,myOrders);
 
 //Admin Routes
-router.route('/orders').get(isAuthenticatedUser,authorizeRoles('admin'), orders);
-router.route('/order/:id').put(isAuthenticatedUser,authorizeRoles('admin'), updateOrder)
+router.route('/admin/orders').get(isAuthenticatedUser,authorizeRoles('admin'), orders);
+router.route('/admin/order/:id').put(isAuthenticatedUser,authorizeRoles('admin'), updateOrder)
                           .delete(isAuthenticatedUser,authorizeRoles('admin'), deleteOrder);
 
 module.exports = router;
